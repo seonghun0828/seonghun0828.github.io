9072cfc36301e53430e22956998b30d5b89cb364
seonghun <pch468@naver.com>
2021-06-14 01:47:53 +0900

    placeholder color, weather https modified

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ const API_KEY = "96c043ee19193abcbca9f152d97d859d";
 const COORDS_LS = 'coords';
 
 function getWeather(lat, lng){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     ).then(function(response){
         return response.json();
     }).then(function(json){
