a7a4ec46776845d0d3688e5cbdf43a13624f16ee
seonghun <pch468@naver.com>
2021-06-18 10:28:48 +0900

    delete name function

--- greeting.js
+++ greeting.js
@@ -1,12 +1,17 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greeting"),
-    greetingToDo = document.querySelector(".js-todo");
+    greetingToDo = document.querySelector(".js-todo"),
+    listDiv = document.querySelector(".list-div");
 
 const USER_LS = "username";
 
 function saveUser(name){
     localStorage.setItem(USER_LS, name);
+    const blindClass = greeting.classList.contains("blind");
+    if(blindClass){
+        greeting.classList.remove("blind");
+    }
 }
 
 function handleInput(event){
@@ -21,11 +26,27 @@ function addUser(){
     form.addEventListener("submit", handleInput);
 }
 
+function deleteName(){
+    localStorage.setItem(USER_LS,"");
+    input.value = "";
+    greeting.classList.add("blind");
+    addUser();
+}
+
+function handleMouse(){
+    greeting.innerText = "You can doubleclick and rename";
+    greeting.addEventListener("mouseleave", loadUser);
+    greeting.addEventListener("dblclick", deleteName);
+}
+
 function loadUser(){
+    console.log("func load");
     const username = localStorage.getItem(USER_LS);
     greeting.innerText = `Hello, ${username}!
-    What do you do today?`;
+What do you do today?`;
+    greeting.addEventListener("mouseover", handleMouse);
     greetingToDo.classList.remove("blind");
+    listDiv.classList.remove("blind");
 }
 
 function checkName(){
