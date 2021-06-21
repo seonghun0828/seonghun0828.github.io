32f6dfae003b645e77c5971a09d13b4eb3ac7362
seonghun <pch468@naver.com>
2021-06-21 10:21:59 +0900

    3s delete alert done

--- greeting.js
+++ greeting.js
@@ -1,51 +1,75 @@
-const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
+const askingName = document.querySelector(".js-form"),
+    input = askingName.querySelector("input"),
+    greetingDiv = document.querySelector(".greeting-div"),
     greeting = document.querySelector(".js-greeting"),
-    greetingToDo = document.querySelector(".js-todo"),
+    askingToDo = document.querySelector(".js-todo"),
     listDiv = document.querySelector(".list-div");
 
 const USER_LS = "username";
 
 function saveUser(name){
     localStorage.setItem(USER_LS, name);
-    const blindClass = greeting.classList.contains("blind");
-    if(blindClass){
+}
+
+function checkAskingName(){
+    const isAskingNameBlind = askingName.classList.contains("blind");
+    if(!isAskingNameBlind){
+        askingName.classList.remove("blind");
+    } 
+    if(isAskingNameBlind) {
         greeting.classList.remove("blind");
+        askingToDo.classList.remove("blind");
     }
 }
 
 function handleInput(event){
     event.preventDefault();
     saveUser(input.value);
+    askingName.classList.add("blind");
+    checkAskingName();
     loadUser();
-    form.classList.add("blind");
 }
 
 function addUser(){
-    form.classList.remove("blind");
-    form.addEventListener("submit", handleInput);
+    askingName.classList.remove("blind");
+    askingName.addEventListener("submit", handleInput);
 }
 
 function deleteName(){
+    removeAlert();
     localStorage.setItem(USER_LS,"");
     input.value = "";
     greeting.classList.add("blind");
+    askingToDo.classList.add("blind");
     addUser();
 }
 
-function handleMouse(){
-    greeting.innerText = "You can doubleclick and rename";
-    greeting.addEventListener("mouseleave", loadUser);
+function alertRename(){
+    const alert = document.createTextNode("You can doubleclick and rename");
+    greetingDiv.prepend(alert);
+}
+
+function removeAlert(){
+    const children = greetingDiv.childNodes;
+    if(children.length === 4) {
+        greetingDiv.removeChild(children[0]);
+    }
+}
+
+function handleInterval(){
+    const children = greetingDiv.childNodes;
+    if(children.length===3){
+        alertRename();
+        setTimeout(removeAlert, 3000);
+    }
     greeting.addEventListener("dblclick", deleteName);
 }
 
 function loadUser(){
-    console.log("func load");
     const username = localStorage.getItem(USER_LS);
-    greeting.innerText = `Hello, ${username}!
-What do you do today?`;
-    greeting.addEventListener("mouseover", handleMouse);
-    greetingToDo.classList.remove("blind");
+    greeting.innerText = `Hello ${username}`;
+    checkAskingName();
+    greeting.addEventListener("mouseover", handleInterval);
     listDiv.classList.remove("blind");
 }
 
